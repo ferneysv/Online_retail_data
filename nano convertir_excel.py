34eb73ab5a51a57d2cd97bd140dda37bf90d7c07
ferneysv <44617778+ferneysv@users.noreply.github.com>
2025-10-25 11:42:51 -0500

    Update nano convertir_excel.py

--- nano convertir_excel.py
+++ nano convertir_excel.py
@@ -1,12 +1,12 @@
 # comando para acceder al nano: nano convertir_excel.py
 # Enlace de descarga desde la fuente original: https://archive.ics.uci.edu/ml/machine-learning-databases/00352/Online%20Retail.xlsx" -O Online_Retail.xlsx
-# comando para ejecutar nano: python3 convertir_excel.py
+# comando para ejecutar: python3 convertir_excel.py
 import pandas as pd
 
-# Lee el archivo Excel (asegúrate de que está en el mismo directorio)
+# Leer el archivo Excel 
 df = pd.read_excel("Online_Retail.xlsx")
 
-# Guarda el archivo como CSV
+# Guardar el CSV
 df.to_csv("online_retail.csv", index=False)
-
+# Salida
 print(" Archivo convertido correctamente a CSV: online_retail.csv")
